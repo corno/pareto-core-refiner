fb3636c061b56c0a15060c65a90901aacc41ab64
Corno Schraverus <corno@schraverus.com>
2025-12-29 10:46:05 +0100

    first commit

---
+++ pub/src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * these functions coming from core-internals should be exposed for deserializer development
+ */
+export {
+    // build_text, probably not needed
+    build_list,
+    build_dictionary,
+    create_iterator,
+    dictionary_literal,
+    list_literal,
+} from "pareto-core-internals"
