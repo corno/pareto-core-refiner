006c46df6ebafc44a9d18a62cb9f8a3b2d3be3a6
Corno Schraverus <corno@schraverus.com>
2025-12-29 12:18:22 +0100

    .

--- pub/src/index.ts
+++ pub/src/index.ts
@@ -1,3 +1,6 @@
+import * as _pinternals from 'pareto-core-internals'
+import * as _pi from 'pareto-core-interface'
+
 /**
  * these functions coming from core-internals should be exposed for deserializer development
  */
@@ -18,3 +21,19 @@ export {
     au,
     ss,
 } from "pareto-core-internals"
+
+export const deprecated_build_dictionary = <T>(
+    $: ($c: _pinternals.Dictionary_Builder<T>) => void
+): _pi.Dictionary<T> => {
+    const temp: { [key: string]: T } = {}
+    $({
+        'add entry': (key, $) => {
+            if (key in temp) {
+                _pinternals.panic(`duplicate key in dictionary literal: ${key}`)
+            }
+            temp[key] = $
+        }
+    })
+    return _pinternals.dictionary_literal(temp)
+}
+
